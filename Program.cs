5164aa66ab0f82f5f6e9a3a551cc5e2d6fd140c5
Bokang214 <lenkoanebokang214@gmail.com>
2026-04-09 10:48:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CYBERBOT_Part_._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
